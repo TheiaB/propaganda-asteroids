ce090be068de095d4cda06ebb089b84dfbf458bb
dagnih <philip.dissert@gmail.com>
2025-06-25 17:32:52 +0200

    Shop and UI stuff

--- scripts/ui/item_sub_viewport.gd
+++ scripts/ui/item_sub_viewport.gd
@@ -1,11 +1,4 @@
 extends SubViewport
-
-@onready var mesh: MeshInstance3D = $MeshInstance3D
-
-func _gui_input(event):
-	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		mesh.rotation_degrees.y += event.relative.x * 0.5
-		mesh.rotation_degrees.x += event.relative.y * 0.5
 		
 @onready var pivot: Marker3D = $Pivot
 
