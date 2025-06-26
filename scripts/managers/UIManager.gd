dedfe82b7b8db26d181f885065c23b987304be07
dagnih <philip.dissert@gmail.com>
2025-06-26 17:54:23 +0200

    somehow make shipmanager work

---
+++ scripts/managers/UIManager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+class_name UIManager
+
+@onready var death_scene: Node2D = $death_scene
+@onready var start_scene: Node2D = $start_scene
+
+func setUI(ui_name: String):
+	death_scene.visible = false
+	start_scene.visible = false
+	if ui_name == "death_scene":
+		death_scene.visible = true
+	
+	if ui_name == "start_scene":
+		start_scene.visible = false
