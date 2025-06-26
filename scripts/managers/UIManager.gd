6130f499a2114f9c3e4a9c2bea7e01144f74e74f
dagnih <philip.dissert@gmail.com>
2025-06-26 17:56:13 +0200

    make parameters of setUI optional

--- scripts/managers/UIManager.gd
+++ scripts/managers/UIManager.gd
@@ -5,7 +5,7 @@ class_name UIManager
 @onready var death_scene: Node2D = $death_scene
 @onready var start_scene: Node2D = $start_scene
 
-func setUI(ui_name: String):
+func setUI(ui_name: String = ""):
 	death_scene.visible = false
 	start_scene.visible = false
 	if ui_name == "death_scene":
