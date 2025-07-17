c009e988a482bda4741e87d3e2019528b58dc1ca
Tea <theialuna+killy-arch@proton.me>
2025-07-17 20:37:37 +0200

    adjusted shooting speed, locked asteroid y axis

--- scripts/projectiles/laser.gd
+++ scripts/projectiles/laser.gd
@@ -1,15 +1,17 @@
 extends Projectile
 
 var initial_velocity
+var bullet_direction
 @onready var sprite: AnimatedSprite3D = $AnimatedSprite3D
 
 func _ready() -> void:
-	initial_velocity = ship.velocity
+	#initial_velocity = ship.velocity.normalized() * speed
 	sprite.play("laser")
 
 func _physics_process(delta: float) -> void:
-	var bullet_direction = -transform.basis.z * speed
-	global_position += (bullet_direction + initial_velocity) * delta
+	bullet_direction = -transform.basis.z
+	#global_position += (bullet_direction + initial_velocity) * delta
+	global_position += (bullet_direction).normalized() * speed * delta
 
 func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 	queue_free()
