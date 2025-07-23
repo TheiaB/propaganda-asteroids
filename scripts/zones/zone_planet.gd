7aa34167257dffa6ce4343fb5ce9b827caf2a637
Tea <theialuna+killy-arch@proton.me>
2025-07-23 12:18:23 +0200

    water planet iterations, planet spin (oops prev commit missed?), planet  size a bit  carying, fixed bug where t́hruster was visible upon spawning

--- scripts/zones/zone_planet.gd
+++ scripts/zones/zone_planet.gd
@@ -4,8 +4,10 @@ class_name ZonePlanet
 
 @export var unique_name: String
 @export var health :int= 3
+@export var rotation_speed :float= -0.05
 
 @export var planet_iterations:Array[Node3D]
+var current_planet_iteration:Node3D
 
 #@export_tool_button("Hurt Me Mommy", "Callable") var hurt_planet = hurt
 func hurt() -> void:
@@ -18,23 +20,27 @@ func modify_health(amount:int = -1):
 		for planet in planet_iterations:
 			planet.hide()
 		if(health > planet_iterations.size()):
-			# health 4 on planet 0
-			planet_iterations[0].show()
+			# health 4+ on planet 0
+			current_planet_iteration = planet_iterations[0]
 			print('---more health than necessary')
 		elif(health < 1):
-			# health 0 on planet 2
+			# health 0- on planet 2
 			print('---less health than necessary')
-			planet_iterations[planet_iterations.size() - 1].show()
+			current_planet_iteration = planet_iterations[planet_iterations.size() - 1]
 		else:
 			# maps
 			# health 3 on planet 0
 			# health 2 on planet 1
 			# health 1 on planet 2
 			print('---health ',health)
-			planet_iterations[planet_iterations.size() - health].show()
+			current_planet_iteration = planet_iterations[planet_iterations.size() - health]
+		current_planet_iteration.show()
 		return
 
 
 func _ready():
 	if(planet_iterations.size()>0):
 		modify_health(0)
+
+func _process(delta: float) -> void:
+	current_planet_iteration.rotate_y(rotation_speed * delta)
