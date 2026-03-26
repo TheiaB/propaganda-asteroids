948f118cf6b16671e6ac8c5697fc91f7a8973c57
evoksontologe <bonzjojo0@gmail.com>
2026-03-26 23:17:32 +0100

    beyblade

---
+++ scripts/items/active_items/active_speed_boost.gd
@@ -0,0 +1,12 @@
+extends Generic_Active_Item
+
+func _ready() -> void:
+	pass
+
+func activate_item(ship: Ship):
+	if ship:
+		pass
+		
+func deactivate_item(ship : Ship):
+	if ship:
+		pass
--- scripts/items/generic_items/passive_speed_boost.gd
+++ scripts/items/generic_items/passive_speed_boost.gd
@@ -1,12 +1,13 @@
 extends Generic_Item
 
-@export var MAX_SPEED := 20
-@export var acc := 9.0
+@export var MAX_SPEED := 9.8
+@export var ACCELERATION := 5.0
+@export var ROTATION_SPEED := 3.0
 
 #func _ready() -> void:
 	
 func load_attributes(ship: Ship):
 	print("loaded boost attributes")
 	ship.stats.MAX_SPEED = MAX_SPEED
-	ship.stats.ACCELERATION = acc
-	ship.stats.ROTATION_SPEED = 6.0
+	ship.stats.ACCELERATION = ACCELERATION
+	ship.stats.ROTATION_SPEED = ROTATION_SPEED
